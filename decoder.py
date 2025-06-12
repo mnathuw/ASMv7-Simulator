94d2cecfaba66b66145d559a6759c3f46fad1b11
Thu Mai <mnathu98@gmail.com>
2025-06-12 10:49:34 -0400

    update decoder.py and encoder.py

--- decoder.py
+++ decoder.py
@@ -1,1 +1,4 @@
 # Binary to assembly decoder
+def Decoder(binary_str):
+    decimal = int(binary_str, 2)
+    return decimal
--- encoder.py
+++ encoder.py
@@ -1,1 +1,33 @@
 # Assembly to binary encoder
+def Encoder(number):
+    if(number >= 0):
+        binary_str = bin(number)
+        binary_32 = binary_str[2:].zfill(32)
+        return binary_32
+
+    else:
+        negative_binary_str = bin(number & 0xFFFFFFFF)
+        negative_binary_32 = negative_binary_str[2:].zfill(32)
+        return negative_binary_32
+
+def Encoder_12bit(number):
+    if(number >= 0):
+        binary_str = bin(number)
+        binary_12 = binary_str[2:].zfill(12)
+        return binary_12
+
+    else:
+        negative_binary_str = bin(number & 0xFFFFFFFF)
+        negative_binary_12 = negative_binary_str[2:].zfill(12)
+        return negative_binary_12
+
+def Encoder_5bit(number):
+    if(number >= 0):
+        binary_str = bin(number)
+        binary_5 = binary_str[2:].zfill(5)
+        return binary_5
+
+    else:
+        negative_binary_str = bin(number & 0xFFFFFFFF)
+        negative_binary_5 = negative_binary_str[2:].zfill(5)
+        return negative_binary_5
